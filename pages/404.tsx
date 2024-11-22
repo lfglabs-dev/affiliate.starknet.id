e76d9b8433c1caa4bab7d476aa132a28a663c37e
OWK50GA <okoriewilfrid.20201206203@futo.edu.ng>
2024-11-22 22:02:58 +0100

    Fix: Refactored 404.tsx page to be consistent across all lfg-labs projects with provided references

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,10 +5,18 @@ import styles from "../styles/Home.module.css";
 const NotFound: NextPage = () => {
   return (
     <div className={styles.screen}>
-      <div className="flex items-center justify-center h-full">
-        <div className="text-center">
-          <h1 className="title">404 - Page Not Found</h1>
-          <p className="mt-2">The page you are looking for does not exist.</p>
+      <div className={styles.wrapperScreen}>
+        <div className="flex flex-col items-center justify-center h-full">
+          <img 
+            src="/visuals/404illu.webp" 
+            alt="error illustration"
+            height={300} 
+            width={300}
+          />
+          <div className="text-center">
+            <h1 className="title">404 - Page Not Found</h1>
+            <p className="mt-2">The page you are looking for does not exist</p>
+          </div>
         </div>
       </div>
     </div>
