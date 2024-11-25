d54b0ca1bee90c5e8dc9b465c3cb97a0a77f1988
Michaelkingsdev <nnajiifeanyi21@gmail.com>
2024-11-25 09:57:01 +0100

    fixes issues #26

--- pages/error.tsx
+++ pages/error.tsx
@@ -1,25 +1,27 @@
 import React, { FunctionComponent } from "react";
+import styles from "../styles/Home.module.css";
+import errorStyle from "../styles/components/error.module.css";
 
-
-const Error: FunctionComponent = () => {
+const NotFound: FunctionComponent = () => {
   return (
-    <div className="flex justify-center  items-center min-h-screen bg-gray-100">
-      <div className="text-center p-8 w-full ">
-        <div className="mb-6">
-          <img
-            src="/visuals/errorIllu.webp"
-            alt="Error illustration: connect to Starknet wallet"
-            className="w-80 h-80 object-contain mx-auto"
+    <div className={`${styles.container}`}>
+      <div className={errorStyle.main}>
+        <div className="flex flex-col items-center justify-center">
+          <img 
+            src="/visuals/errorillu.webp" 
+            alt="error illustration"
+            height={300} 
+            width={300}
           />
+          <div className="text-center">
+            <h1 className="title text-center">Oh no... access error!</h1>
+            <p className="mt-2">To access to your stark affiliate space you need to connect to a
+            starknet wallet</p>
+          </div>
         </div>
-
-        <h2 className="text-4xl font-semibold  text-center">Oh no... access error!</h2>
-        <p className="mt-4 text-lg text-gray-700">
-          To access your Stark affiliate space, you need to connect to a Starknet wallet.
-        </p>
       </div>
     </div>
   );
 };
 
-export default Error;
+export default NotFound;
