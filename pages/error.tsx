ba7e378a03349a2c09a881e3a0af644a862d9f23
Michaelkingsdev <125934142+Michaelkingsdev@users.noreply.github.com>
2024-11-28 02:52:04 +0100

    Fix 26 (#30)
    * Make our error.tsx page consistent
    
    * Make our error.tsx page consistent
    
    * adjustment
    
    * Fix: Updated Navbar and  button styles (#31)
    
    * Fix #22 : Refont of FAQ section (#27)
    
    * git commit -m "Fix: Refont of FAQ section"
    
    * updated changes
    
    * fix:build
    
    * git commit -m "Fix: Refont of FAQ section"
    
    * updated changes
    
    * updated code
    
    * Text center removed
    
    ---------
    
    Co-authored-by: Fricoben <benjamin.flores.pro@gmail.com>
    
    * Fix: Refactored 404.tsx page to be consistent across all lfg-labs projects with provided references (#28)
    
    * Fix: Refactored 404.tsx page to be consistent across all lfg-labs projects with provided references
    
    * centering fixed, colour fixed
    
    * Fix Stark Affiliate section (#29)
    
    * fix affiliate link
    
    * implement design
    
    * use green tick and change link
    
    * Make our error.tsx page consistent
    
    * Make our error.tsx page consistent
    
    * adjustment
    
    * fixes issues #26
    
    * fix merge conflict
    
    * change errorillu to errorIllu
    
    * modify error page description
    
    ---------
    
    Co-authored-by: Lau Chaves <laurens004@gmail.com>
    Co-authored-by: Sudipta Paul <117905151+SudiptaPaul-31@users.noreply.github.com>
    Co-authored-by: Fricoben <benjamin.flores.pro@gmail.com>
    Co-authored-by: Wilfrid Okorie <114712580+OWK50GA@users.noreply.github.com>
    Co-authored-by: wheval <whevalmezeegbe@gmail.com>

--- pages/error.tsx
+++ pages/error.tsx
@@ -2,18 +2,27 @@ import React, { FunctionComponent } from "react";
 import styles from "../styles/Home.module.css";
 import errorStyle from "../styles/components/error.module.css";
 
-const Error: FunctionComponent = () => {
+
+const NotFound: FunctionComponent = () => {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container}`}>
       <div className={errorStyle.main}>
-        <h2>Oh no... access error!</h2>
-        <p className="text-small mt-8">
-          To access to your stark affiliate space you need to connect to a
-          starknet wallet
-        </p>
+        <div className="flex flex-col items-center justify-center">
+          <img 
+            src="/visuals/errorIllu.webp" 
+            alt="error illustration"
+            height={300} 
+            width={300}
+          />
+          <div className="text-center">
+            <h1 className="title text-center">Oh no... access error!</h1>
+            <p className="mt-2">To access your stark affiliate space, you need to connect to a
+            starknet wallet</p>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Error;
+export default NotFound;
