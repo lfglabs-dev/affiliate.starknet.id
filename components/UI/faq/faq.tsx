fc97460d313f4ee1cc495e676f5c14cc2c927006
Wilfred007 <adzerwilfred007@gmail.com>
2025-03-24 13:28:43 +0100

    fix: fixed frequently asked questions section to fit design

--- components/UI/faq/faq.tsx
+++ components/UI/faq/faq.tsx
@@ -9,13 +9,12 @@ interface FaqBoardProps {
 
 export const FaqBoard: FC<FaqBoardProps> = ({ faq }) => {
 	return (
-		<div className="w-full max-w-[1184px]  mx-auto bg-[#FEF7F1] rounded-lg p-8 overflow-hidden">
+		<div className="w-full max-w-[1184px]  mx-auto bg-[#FEF7F1] rounded-lg p-8 overflow-hidden mb-10">
 			<div className="mb-8">
-				<p className="text-sm text-gray-600 mb-2 mt-2">Find quick answers</p>
 				<h1 className="text-3xl font-extrabold text-center">FREQUENTLY ASKED QUESTION</h1>
 			</div>
 
-			<div className="space-y-6 shadow-[0px_14px_39px_0px_rgba(0,0,0,0.1)] px-[24px] pt-[24px]">
+			<div className="space-y-6 shadow-[0px_14px_39px_0px_rgba(0,0,0,0.1)] px-[24px] pt-[24px] mb-10">
 				{faq.map((faq, index) => (
 					<Accordion
 						key={index}
@@ -35,7 +34,7 @@ export const FaqBoard: FC<FaqBoardProps> = ({ faq }) => {
 							}
 							className="p-0 hover:bg-transparent min-h-0">
 							<div className="flex items-center gap-4">
-								<span className="text-2xl font-bold text-gray-800 min-w-[40px]">{String(index + 1).padStart(2, "0")}</span>
+								<h1 className="text-3xl font-extrabold text-center">{String(index + 1).padStart(2, "0")}</h1>
 								<p className="text-lg font-medium text-gray-800">{faq.question}</p>
 							</div>
 						</AccordionSummary>
