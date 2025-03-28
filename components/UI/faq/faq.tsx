d759ddb64268734c54f1d24e1ca199122972948e
Wilfred007 <adzerwilfred007@gmail.com>
2025-03-28 17:49:29 +0100

    Fix: fixed the frequently asked question section to fit the design

--- components/UI/faq/faq.tsx
+++ components/UI/faq/faq.tsx
@@ -9,39 +9,40 @@ interface FaqBoardProps {
 
 export const FaqBoard: FC<FaqBoardProps> = ({ faq }) => {
 	return (
-		<div className="w-full max-w-[1184px]  mx-auto bg-[#FEF7F1] rounded-lg p-8 overflow-hidden mb-10">
+		<div className="w-11/12  mx-auto bg-[#ffffff] rounded-lg p-8 overflow-hidden mb-10">
 			<div className="mb-8">
 				<h1 className="text-3xl font-extrabold text-center">FREQUENTLY ASKED QUESTION</h1>
 			</div>
 
 			<div className="space-y-6 shadow-[0px_14px_39px_0px_rgba(0,0,0,0.1)] px-[24px] pt-[24px] mb-10">
-				{faq.map((faq, index) => (
-					<Accordion
-						key={index}
-						className="bg-transparent shadow-none before:hidden"
-						sx={{
-							"&::before": {
-								backgroundColor: "#000",
-							},
-						}}
-						disableGutters={true}
-						square>
-						<AccordionSummary
-							expandIcon={
-								<div className="w-6 h-6 flex items-center justify-center border border-gray-600 rounded-md ">
-									<PlusIcon width="10" />
+				{faq.slice(0, -1).map((faq, index) => (
+					<div key={index} className="border-b border-gray-100 pb-4">
+						<Accordion
+							className="bg-transparent shadow-none before:hidden"
+							sx={{
+								"&::before": {
+									backgroundColor: "#000",
+								},
+							}}
+							disableGutters={true}
+							square>
+							<AccordionSummary
+								expandIcon={
+									<div className="w-6 h-6 flex items-center justify-center border border-gray-600 rounded-md ">
+										<PlusIcon width="10" />
+									</div>
+								}
+								className="p-0 hover:bg-transparent min-h-0">
+								<div className="flex items-center gap-4">
+									<h1 className="text-3xl font-extrabold text-center">{String(index + 1).padStart(2, "0")}</h1>
+									<p className="text-lg font-medium text-gray-800">{faq.question}</p>
 								</div>
-							}
-							className="p-0 hover:bg-transparent min-h-0">
-							<div className="flex items-center gap-4">
-								<h1 className="text-3xl font-extrabold text-center">{String(index + 1).padStart(2, "0")}</h1>
-								<p className="text-lg font-medium text-gray-800">{faq.question}</p>
-							</div>
-						</AccordionSummary>
-						<AccordionDetails className="pl-[64px] pt-2 pb-4">
-							<p className="text-gray-600 text-left">{faq.answer}</p>
-						</AccordionDetails>
-					</Accordion>
+							</AccordionSummary>
+							<AccordionDetails className="pl-[64px] pt-2 pb-4">
+								<p className="text-gray-600 text-left">{faq.answer}</p>
+							</AccordionDetails>
+						</Accordion>
+					</div>
 				))}
 			</div>
 		</div>
